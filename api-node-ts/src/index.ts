71e130f3f897256c520c301801f6f6313a967c17
jessi <jessicanicodemos@gmail.com>
2023-02-05 22:28:58 -0300

    commit

--- api-node-ts/src/index.ts
+++ api-node-ts/src/index.ts
@@ -3,10 +3,10 @@ import { router } from './routes';
 
 const server = express();
 
+server.use(express.json())
 server.use(router)
 
 server.listen(5000, () => {
     console.log('Servidor on na porta 5000 http://localhost:5000/')
 })
 
-Continuar em 10m
--- api-node-ts/src/routes.ts
+++ api-node-ts/src/routes.ts
@@ -1,25 +1,14 @@
 import { Router, Request, Response } from "express";
+import { CreateUsersController } from "./Controllers/CreateUsersController";
 
 const router = Router();
+const creatUsersController = new CreateUsersController();
 
 router.get('/', (request: Request, response: Response) => {
 
     return response.json({mensagem: 'Bem vindo a nossa DIO API'})
 })
 
-router.get('/usuarios', (request: Request, response:Response) => {
-
-return response.json([
-        {
-            nome: 'Jessica'
-        },
-        {
-            nome: 'Leonardo'
-        },
-        {
-            nome: 'Theo'
-        }
-    ])
-})
+router.post('/usuarios', creatUsersController.handle)
 
 export { router }
